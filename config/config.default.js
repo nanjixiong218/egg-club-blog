b5747a61eddd6ac78613507c56b0a3e8ff8b980a
许会元 <xuhy05@jcgroup.com.cn>
2018-07-18 14:43:06 +0800

    fixed : prot

--- config/config.default.js
+++ config/config.default.js
@@ -5,7 +5,7 @@ module.exports = appInfo => {
   const config = {};
   config.cluster = {
     listen: {
-      port: 80,
+      port: 8000,
       hostname: '127.0.0.1',
     // path: '/var/run/egg.sock',
     },
